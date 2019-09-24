834961239a5668f35147e6dd8e6d363dee6995bd
Adam Jacoby <majacoby8202@gmail.com>
2019-09-24 15:29:15 -0500

    Update Solution.java

--- JAVA/Challenges/Train Triple Trouble/Solution.java
+++ JAVA/Challenges/Train Triple Trouble/Solution.java
@@ -1,3 +1,4 @@
+
 public class Kata
 {
   public static int TripleDouble(long num1, long num2) 
@@ -42,3 +43,19 @@ public class Kata
     return ret;
   }
 }
+/* Improved Version:
+
+public class Kata
+{
+  public static int TripleDouble(long num1, long num2) 
+  {
+    String n1str = String.valueOf(num1);
+    String n2str = String.valueOf(num2);
+    for(int i=0;i<10;i++) {
+      String n = String.valueOf(i);
+      if( n1str.contains(n+n+n) && n2str.contains(n+n) ) return 1;
+    }
+    return 0;
+  }
+}
+*/
